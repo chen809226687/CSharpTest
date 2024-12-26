263df3f38f19522b16ad77f615537ed31e86cc0c
chenzeyuan <chenzeyuan@sinsegye.com.cn>
2024-12-26 10:14:35 +0800

    初次提交

---
+++ src/WindowsFormsTest/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.Attribute = new System.Windows.Forms.TabControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage1.SuspendLayout();
+            this.Attribute.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(676, 327);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(676, 327);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Attribute";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // Attribute
+            // 
+            this.Attribute.Controls.Add(this.tabPage1);
+            this.Attribute.Controls.Add(this.tabPage2);
+            this.Attribute.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Attribute.Location = new System.Drawing.Point(0, 0);
+            this.Attribute.Name = "Attribute";
+            this.Attribute.SelectedIndex = 0;
+            this.Attribute.Size = new System.Drawing.Size(684, 353);
+            this.Attribute.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 353);
+            this.Controls.Add(this.Attribute);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabPage1.ResumeLayout(false);
+            this.Attribute.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl Attribute;
+    }
+}
+
---
+++ src/WindowsFormsTest/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+        #region 特性
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OldMethod();
+        }
+
+
+        [Obsolete("OldMethod 已弃用，请改用 NewMethod", false)]
+        void OldMethod()
+        {
+            Console.WriteLine("已弃用的函数");
+        }
+        void NewMethod()
+        {
+            Console.WriteLine("新定义的函数");
+        }
+        #endregion
+    }
+}
